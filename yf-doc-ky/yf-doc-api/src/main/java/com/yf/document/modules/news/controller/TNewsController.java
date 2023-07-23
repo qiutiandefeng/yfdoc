85bf4a00f038aaff143d4195cfd0000ccb8abcab
云帆互联 <xiaoquan328@163.com>
2023-07-24 07:36:38 +0800

    Update TNewsController.java
    提交更新

--- yf-doc-ky/yf-doc-api/src/main/java/com/yf/document/modules/news/controller/TNewsController.java
+++ yf-doc-ky/yf-doc-api/src/main/java/com/yf/document/modules/news/controller/TNewsController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
-
 /**
 * <p>
 * 新闻列表控制器
