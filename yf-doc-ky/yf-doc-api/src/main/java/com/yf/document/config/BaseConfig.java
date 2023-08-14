d9467608afc7ae90b68e4358a0984f5c2ce0551c
云帆互联 <xiaoquan328@163.com>
2023-08-14 09:33:40 +0800

    Update BaseConfig.java
    更新

--- yf-doc-ky/yf-doc-api/src/main/java/com/yf/document/config/BaseConfig.java
+++ yf-doc-ky/yf-doc-api/src/main/java/com/yf/document/config/BaseConfig.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-
 /**
  * 全局静态配置
  * @author bool
